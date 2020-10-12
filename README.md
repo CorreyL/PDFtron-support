c192b30a3c39ede773dd641b719c124eb0020e3d
Roy Ermers <roy@roy-daan.nl>
2020-10-12 15:53:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# pdftron support
+
+## The problem
+
+I want to view a pdf in my nuxt application. I know nuxt is serverside so I tried to render the webviewer client side. But that didn't work
+
+## Run this program
+
+use `npm run dev` to run a local webserver hosting this vue project.
---
+++ components/PDFViewer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div ref="viewer" />
+</template>
+
+<script>
+import WebViewer from '@pdftron/webviewer';
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true
+    }
+  },
+
+  mounted () {
+    //   return;
+
+    const instance = WebViewer({
+
+      path: '/webviewer',
+      initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf'
+    }, this.$refs.viewer.$el
+    );
+    console.log(instance);
+  }
+}
+</script>
+
+<style>
+
+</style>
